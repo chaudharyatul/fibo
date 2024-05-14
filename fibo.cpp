25aa56d240ac3d043f2370b6868f76b5069172b3
ATUL CHAUDHARY <91599918+chaudharyatul@users.noreply.github.com>
2024-05-14 15:15:05 +0530

    Add files via upload

---
+++ fibo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a,b,c,d=0,e=1;
+
+    cout<<"enter divident b=";
+    cin>>b;
+    for(a=1; a<=b; a++)
+    {
+        cout<<d;
+        c=d+e;
+        d=e;
+        e=c;
+        
+    }
+
+    return 0;
+}
